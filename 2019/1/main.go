b65051d119e5802f1d93a836d92c4e2d53952fca
Christophe Boucharlat <christophe@containo.us>
2019-12-03 17:49:27 +0100

    Day One - part 2

--- 2019/1/main.go
+++ 2019/1/main.go
@@ -7,8 +7,13 @@ import (
 	"os"
 )
 
+func fuelCalc(mass int)(fuelMass, leftMass int){
+	return mass / 3 - 2, mass % 3
+}
+
 func main(){
 	f, err := os.Open("input.txt")
+	//f, err := os.Open("input-validated.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,13 +23,30 @@ func main(){
 
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-		var fuelModule int
+		var fuelModule, modulo, fuelMass, fuelSum int
 		_, err = fmt.Sscanf(s.Text(), "%d", &fuelModule)
 		if err != nil {
 			log.Fatalf("could not read value: %s, err", s.Text(), err )
 		}
-		divided := fuelModule / 3 - 2
-		sum = sum + divided
+
+		// Initial calculation of the fuelMass added for the module mass. and the modulo.
+		fuelMass, modulo = fuelCalc(fuelModule)
+
+		// Adding the fuelMass to the fuelSum to get the initial mass of fuel for this module
+		fuelSum = fuelMass
+
+		// Calculating the amount of fuelMass to add to support itself.
+		// While fuelMass is positive and over 0 calculate the required amount of Fuel for it.
+		for fuelMass > 0 {
+			fuelMass, modulo = fuelCalc(fuelMass)
+			// If fuelMass is positive and over 0 Add the fuel Mass to the sum of fuelMass.
+			if fuelMass > 0 {
+				fuelSum = fuelSum + fuelMass
+			}
+		}
+
+		// Adding the fuelSum for the module to the shared sum.
+		sum = sum + fuelSum
 	}
 	if err := s.Err(); err != nil {
 		log.Fatal(err)
